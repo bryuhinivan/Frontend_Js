8db80ad03b4fa25464a9da796c7f078c8f48ab18
bryuhin.ivan <bryuhin.ivan@gmail.com>
2025-04-27 13:06:51 +0500

    added alternate solution additiveNumber

--- tasks-for-practice/opposite-number.js
+++ tasks-for-practice/opposite-number.js
@@ -1,7 +1,15 @@
 /* Очень просто: дано число (целое / десятичное / и то, и другое в зависимости от языка),
 найти его противоположность (аддитивное обратное число). */
 
-function oppositeNum(number) {
+
+function additiveNumber (number) {
+  return -number;
+}
+
+console.log(additiveNumber(-0.12));
+
+
+/* function oppositeNum(number) {
   if (number > 0 || number < 0) {
     return -number;
   } else {
@@ -13,10 +21,11 @@ console.log(oppositeNum(23));
 console.log(oppositeNum(-5));
 console.log(oppositeNum(0));
 console.log(oppositeNum(0.25));
-console.log(oppositeNum(-0.57));
+console.log(oppositeNum(-0.57)); */
 
 
 // Альтернативное решение
 /* function oppositeNum(number) {
   return number !== 0 ? -number : 0
 } */ 
+
