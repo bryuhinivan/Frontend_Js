97bb83b516916445eeed97b52b8df536f86c2933
bryuhin.ivan <bryuhin.ivan@gmail.com>
2025-05-01 13:16:17 +0500

    added alternative solution filtelist

--- tasks-for-practice/listfiltering.js
+++ tasks-for-practice/listfiltering.js
@@ -6,8 +6,16 @@ filter_list([1,2,'a','b']) == [1,2]
 filter_list([1,'a','b',0,15]) == [1,0,15]
 filter_list([1,2,'aasf','1','123',123]) == [1,2,123] */
 
-const mixedArray = [1, 2, 'a', 'b'];
+/* const mixedArray = [1, 2, 'a', 'b'];
 
 const number = mixedArray.filter(item => typeof item === 'number');
-console.log(number);
+console.log(number); */
 
+const arrayStrAndNum = [1, 2, 'aasf', '1', '123', 123];
+
+function arrData(arr) {
+  const filterList = arr.filter(num => typeof num === 'number');
+  return filterList;
+}
+
+console.log(arrData(arrayStrAndNum));
