e7a0b21c4c87de60406ed8bda4c8e50706f14270
bryuhin.ivan <bryuhin.ivan@gmail.com>
2025-05-08 17:16:25 +0500

    modification of the solution for improved readability

--- tasks-for-practice/calcSum.js
+++ tasks-for-practice/calcSum.js
@@ -7,20 +7,11 @@ const purchases = [
   { item: 'Keyboard', price: 100 },
 ];
 
-
-/* function countProducts(arr){
-  const itemArr = arr.map(item => item.price);
-  console.log(itemArr);
-  const sum = itemArr.reduce((acc, num) => acc + num, 0);
-  console.log(sum);
-}
-
-countProducts(purchases); */
-
 function purchasesSum(arr) {
-  const arrElements = arr.map(element => element.price);
-  const elementsSum = arrElements.reduce((acc, element) => acc + element, 0)
-  return elementsSum;
+  const sumElements = arr
+    .map(element => element.price) // [1000, 500, 150, 100]
+    .reduce((acc, num) => acc + num, 0); // 1750
+  return sumElements;
 }
 
 console.log('Сумма всех покупок:', purchasesSum(purchases), 'руб.');
